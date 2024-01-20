c111c007af3537356bbbfd9b0014ee0570b63251
EternalBliss <67732111+EternalBlissed@users.noreply.github.com>
2024-01-21 10:19:33 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,43 @@
 #! /bin/bash
 set -e
 
+# Create necessary directories and copy the wallpaper
 mkdir -p $HOME/.config/
 sudo mkdir -p /usr/share/backgrounds/cnzn/
 sudo cp wallpaper/wallpaper.jpg /usr/share/backgrounds/cnzn/wallpaper.jpg
 
+# Update the system and install necessary packages
 sudo pacman -Syu
+sudo pacman -S upower gvfs pulseaudio pulseaudio-alsa pulseaudio-jack pulseaudio-zeroconf  pulseaudio-bluetooth nano thunar mpv dconf dunst zsh xorg-xrandr brightnessctl bluez blueman hsetroot sxhkd picom ttf-jetbrains-mono network-manager-applet xorg-xsetroot pavucontrol python-setuptools xorg-server xorg-xinit zsh-completions
 
-sudo pacman -S upower gvfs pulseaudio pulseaudio-alsa pulseaudio-jack pulseaudio-zeroconf  pulseaudio-bluetooth nano thunar mpv dconf dunst zsh xorg-xrandr brightnessctl bluez blueman hsetroot sxhkd picom ttf-jetbrains-mono network-manager-applet xorg-xsetroot pavucontrol python-setuptools xorg-server xorg-xinit
-
+# Enable and start necessary services
 sudo systemctl enable bluetooth NetworkManager upower
 sudo systemctl start bluetooth NetworkManager upower
 
+# Prepare for AUR package installation
 mkdir -p AURPackages
 cd AURPackages
-
 git clone https://aur.archlinux.org/trizen
 cd trizen && makepkg -si
 
-trizen -S catppuccin-gtk-theme-mocha papirus-icon-theme-git adwaita-cursors-git vscodium-bin screenfetch
+# Prompt for optional tool installation
+echo "Would you like to install extra tools (vscodium, obsidian-bin, screenfetch, rustup, go, audacity, btop, intel-ucode, neovim, nasm, docker, docker-compose, tree, pacman-contrib, nodejs, npm, unzip)? (y/n)"
+read -r install_extra
 
+if [ "$install_extra" = "y" ]; then
+    trizen -S vscodium-bin obsidian-bin screenfetch rustup go audacity btop intel-ucode neovim nasm docker docker-compose tree pacman-contrib nodejs npm unzip
+fi
+
+# Prompt for optional tool installation
+echo "Do you have a nvidia GPU? (y/n)"
+read -r install_gpu
+
+if [ "$install_gpu" = "y" ]; then
+    sudo pacman -S nvidia-dkms
+fi
+
+# Continue with other installations
+trizen -S catppuccin-gtk-theme-mocha papirus-icon-theme-git adwaita-cursors-git
 trizen -S --skipinteg librewolf-bin
 
 cd ../
@@ -30,8 +48,8 @@ cd ../
 git clone https://aur.archlinux.org/volctl.git
 cd volctl && makepkg -si
 
-# Change from ../dwm to dwm if above is commented out
-cd dwm && sudo make install
+# Install window manager and related tools
+cd ../dwm && sudo make install
 sudo make clean
 
 cd ../st && sudo make install
@@ -40,12 +58,12 @@ sudo make clean
 cd ../dmenu && sudo make install
 sudo make clean
 
+# Configure sxhkd and dunst
 cd ../ && cp -r sxhkd/ $HOME/.config/ 
-
 cp -r dunst/ $HOME/.config/
-
 chmod +x $HOME/.config/dunst/launch.sh
 
+# Set up .xinitrc
 cd $HOME && touch .xinitrc
 echo 'exec dwm
 ./$HOME/.config/dunst/launch.sh
@@ -53,12 +71,13 @@ xrandr --output eDP-1 --mode 1920x1080
 xrandr --output DP-1 --mode 1920x1080 --right-of eDP-1
 ' > .xinitrc 
 
+# Set up .zshrc
 echo 'alias codium=code
 alias nvim=vim
 ' > .zshrc
 
+# Configure GTK settings
 mkdir ~/.config/gtk-3.0
-
 echo '[Settings]
 gtk-theme-name=Catppuccin-Mocha-Standard-Lavender-Dark
 gtk-icon-theme-name=Papirus-Dark
@@ -74,8 +93,11 @@ gtk-enable-input-feedback-sounds=1
 gtk-xft-antialias=1
 gtk-xft-hinting=1
 gtk-xft-hintstyle=hintfull' > ~/.config/gtk-3.0/settings.ini
+
+# Clean up package cache
 trizen -Scc
 
 echo "Finished, Please Reboot after changing your shell"
 
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
