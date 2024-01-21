f48826bf4a134b840fd566904ae804b871a1bea0
EternalBliss <67732111+EternalBlissed@users.noreply.github.com>
2024-01-21 12:19:54 +1100

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
     "hsetroot", "-cover", "/usr/share/backgrounds/cnzn/wallpaper.jpg", NULL,
-    "sh", "-c", "while true; do max_brightness=$(brightnessctl m); xsetroot -name \"$(date +%I:%M) $(brightnessctl g | awk -v max=$max_brightness '{printf \"%d%%\", $1*100/max}') Battery: $(cat /sys/class/power_supply/BAT0/capacity)%\"; sleep 1; done", NULL,
+    "sh", "-c", "while true; do max_brightness=$(brightnessctl m); xsetroot -name \"$(date +%I:%M) \\uF0EB $(brightnessctl g | awk -v max=$max_brightness '{printf \"%d%%\", $1*100/max}') \\uF240 $(cat /sys/class/power_supply/BAT0/capacity)%\"; sleep 1; done", NULL,
     "volctl", NULL,
     "nm-applet", NULL,
     "blueman-applet", NULL,
